a7c292a0672c70c6b5e4ebd92136aa01676b796c
AWSANIMA <pieteroehlers@gmail.com>
2019-10-21 19:13:28 +0200

    fg

--- Fall/Fall.js
+++ Fall/Fall.js
@@ -2,6 +2,7 @@ var cubeX;
 var mouseEnX = 250;
 var enY = 25;
 var score = 0;
+var e;
 
 function setup() {
 	createCanvas(500, 500);
@@ -13,9 +14,10 @@ function draw() {
 	fill(255, 0, 0);
 	cubeX = constrain(mouseX, 25, width- 25);
 	rect(cubeX, height - 25, 50, 50);
-	rect(mouseEnX, enY, 50, 50);
+	e = new enemy();
 	enY = enY + 5;
 	mousePosXEn();
+	collcheck();
 	textSize(24);
 	text('Score: ' + score, width - 490 , 25);
 }
@@ -26,5 +28,20 @@ function mousePosXEn() {
 		mouseEnX = cubeX;
 		enY = 25;
 		score = score + 1;
+	}
+}
+
+function enemy() {
+	rect(mouseEnX, enY, 50, 50);
+}
+
+function collcheck() {
+	if (enY <= height - 25 && enY >= height - 50 ) {
+		if ( cubeX >= mouseEnX - 25 && cubeX <= mouseEnX + 25) {
+			score = 0;
+			enY = 25;
+			mouseEnX = 250;
+			print('frick');
+		}
 	}
 }
