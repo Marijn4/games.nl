96f161e114783158e23cfa5224adb88b87c0c4b7
Marijn4 <marijn.bakkum@gmail.com>
2019-10-28 09:40:25 +0100

    highscore

--- index.js
+++ index.js
@@ -2,4 +2,5 @@ window.onload = function() {
 	document.getElementById("hSnake").innerHTML = "SNAKE: " + localStorage.getItem('highscore');
 	document.getElementById("hSlide").innerHTML = "SLIDE: " + localStorage.getItem('highscore1');
 	document.getElementById("hJump").innerHTML = "JUMP: " + localStorage.getItem('highscore2');
+	document.getElementById("hFall").innerHTML = "FALL: " + localStorage.getItem('highscore3');
 }
